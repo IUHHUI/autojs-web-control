7aaafbecc1251e2d101be7add2e2e3312423db48
awamwang <wm.keepgoing@gmail.com>
2023-09-22 11:09:35 +0800

    fix: 使用编程方式来控制model alias

--- server/src/modules/default/main.ts
+++ server/src/modules/default/main.ts
@@ -2,7 +2,22 @@
  * @Author: kun
  * 2019-10-25
  */
-require('module-alias/register');
+// require('module-alias/register');
+const moduleAlias = require('module-alias');
+// moduleAlias.addAlias('@', __dirname + '../../')
+moduleAlias.addPath(__dirname + '../../../lib')
+moduleAlias.addAlias('@', (fromPath, request, alias) => {
+  // fromPath - Full path of the file from which `require` was called
+  // request - The path (first argument) that was passed into `require`
+  // alias - The same alias that was passed as first argument to `addAlias` (`@src` in this case)
+
+  // Return any custom target path for the `@src` alias depending on arguments
+  // if (fromPath.startsWith(__dirname + '/others')) return __dirname + '/others'
+  // console.log('addAlias', fromPath, request)
+  if (/node_modules/.test(fromPath)) return __dirname
+
+  return __dirname + '../../../'
+})
 import * as koaLogger from 'koa-logger';
 import { createApplication } from '@/common/application';
 import { NODE_ENV } from '@/utils/enums';
