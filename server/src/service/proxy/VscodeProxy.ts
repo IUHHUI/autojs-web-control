bf9ee125c502bd9c66208a246730d1f71ba106f6
awamwang <wm.keepgoing@gmail.com>
2023-09-25 23:54:29 +0800

    fix(server-func): VscodeProxy打印改动

--- server/src/service/proxy/VscodeProxy.ts
+++ server/src/service/proxy/VscodeProxy.ts
@@ -31,7 +31,7 @@ export class VscodeProxy {
     client.connect(this.targetUrl);
 
     client.on('connect', (connection: websocket.connection) => {
-      logger.info(`VscodeProxy try connect -> ${this.targetUrl}`);
+      logger.info(`VscodeProxy connected to -> ${this.targetUrl}`);
       for (const id in this.clientConnectListeners) {
         const listener = this.clientConnectListeners[id];
         listener(connection);
